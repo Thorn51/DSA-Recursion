ae25e293c0754c193188cf4d3cb0804bfa118637
Matt Thornberry <mtberry16@hotmail.com>
2020-03-04 09:36:46 -0700

    Remove call

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //Sum numbers from start to end of list-.
 function recursiveSumOf(list) {
   debugger;
-  if (list.length === 1){return list[0];} //escape-.
-  let listTail = list.slice(1); //slice from 1 to end-. 
+  if (list.length === 1) {
+    return list[0];
+  } //escape-.
+  let listTail = list.slice(1); //slice from 1 to end-.
   let sum = list[0] + recursiveSumOf(listTail); //recursion-.
   return sum;
 }
 
 let testList = [2, 4, 6, 8, 10];
-recursiveSumOf(testList);

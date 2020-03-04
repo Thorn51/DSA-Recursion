6fe0c55d701e0c38f7cd418c58b5698c876693aa
Matt Thornberry <mtberry16@hotmail.com>
2020-03-04 13:55:55 -0700

    Complete Power Calculator exercise

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ function recursiveSumOf(list) {
 
 let testList = [2, 4, 6, 8, 10];
 
-//Exercise 1 -> Counting Sheep
+// Exercise 1 -> Counting Sheep
 
-//Input -> number of sheep (n)
-//Exit Case -> n === 0
+// Input -> number of sheep (n)
+// Exit Case -> n === 0
 // Recursive Input -> ${n}-1
 // Recursive Output -> n: + Another sheep jumps over the fence
 // Exit Output -> All of the sheep jumped over the fence
@@ -29,3 +29,36 @@ function recursiveCountSheep(n) {
   console.log(`${n}: Another sheep jumps over the fence`);
   recursiveCountSheep(n - 1);
 }
+
+// Exercise 2 -> Power Calculator
+
+// Input -> base(integer), power(integer)
+// Exit Case -> power === 0
+// Recursive Input -> base, power-power
+// Recursive Output -> base^power
+// Exit Output -> None
+
+function recursivePowerCalculator(base, power) {
+  //Return if power parameter is negative
+  if (power < 0) {
+    return `Exponent should be >= 0`;
+  }
+
+  //Base condition -> Exit condition
+  if (power === 0) {
+    return console.log("Calculation finished");
+  }
+
+  //Recursive Case
+  recursivePowerCalculator(base, power - power);
+  let result = base ** power;
+  return `${base}^${power} = ${result}`;
+}
+
+// Exercise
+
+// Input ->
+// Exit Case ->
+// Recursive Input ->
+// Recursive Output ->
+// Exit Output ->

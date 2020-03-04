5971722b1321c8f62b90eb92f9b5eae1c325c0a1
Matt Thornberry <mtberry16@hotmail.com>
2020-03-04 09:50:46 -0700

    Add sheep exercise

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function recursiveSumOf(list) {
 }
 
 let testList = [2, 4, 6, 8, 10];
+
+//Exercise 1 -> Counting Sheep
+//Input -> number of sheep
+// Output -> number of sheet + message
+// Recursive Input -> Number of sheep -1
+//
+
+function recursiveCountSheep(n) {
+  //Base Case -> Exit condition
+  if (n === 0) {
+    return "All of sheep jumped over the fence.";
+  }
+
+  //Recursive Case
+  return (
+    `${n}: Another sheep jumps over the fence` + recursiveCountSheep(n - 1)
+  );
+}
